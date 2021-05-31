a0dd75497b71541d71edae6e2c2c62fde5e997a8
JoanPC1 <jplacevedo@gmail.com>
2021-05-31 04:33:05 -0600

    Second commit

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const app = express();
 
 app.use("/admin", admin);
 app.use("/", client);
-
+app.use(express.static(__dirname + "/public"));
 app.listen(3000);
 
--- public/js/admin.js
+++ public/js/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require("path");
 
 const checkSecurity = (req, res, next) => {
-    console.log("Checking security");  //Do securitycheck here
+    console.log("Checking security");  
   
     next();
   };
@@ -24,12 +24,45 @@ router.get("/cs0", checkSecurity, function(req, res) {
     res.sendFile(path.join(__dirname, "../html/cs0.html"));
 });
 
-router.get("/ap", checkSecurity, function(req, res) {
+router.get("/app", checkSecurity, function(req, res) {
     res.sendFile(path.join(__dirname, "../html/applications.html"));
 });
 
 router.get("/ap0", checkSecurity, function (req, res) {
     res.sendFile(path.join(__dirname, "../html/ap0.html"));
 });
+
+router.get("/createaccount", function (req,res) {
+    res.sendFile(path.join(__dirname, "../html/createaccount1.html"));
+});
+
+//add new account handler
+router.post("/submitaccount", function (req,res) {
+    res.sendFile(path.join(__dirname, "../html/createaccount2.html"));
+});
+
+//add email confirmation handler
+router.post("/confirmemail", function (req,res) {
+    res.sendFile(path.join(__dirname, "../html/createaccount3.html"));
+});
+
+router.get("/forgotpassword", function (req,res) {
+    res.sendFile(path.join(__dirname, "../html/password1.html"));
+});
+
+//add password retrieval code
+router.post("/submitemail", function (req,res) {
+    res.sendFile(path.join(__dirname, "../html/password2.html"));
+});
+
+router.get("/enterpassword", function (req,res) {
+    res.sendFile(path.join(__dirname, "../html/password3.html"));
+});
+
+//add change password handler here
+router.post("/passwordconfirm", function (req,res) {
+    res.sendFile(path.join(__dirname, "../html/password4.html"));
+});
+
 
 module.exports = router;
